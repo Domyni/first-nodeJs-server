c4c21207aee75541d2409ba599f752588a901eaa
iAmWasin <52755109+Domyni@users.noreply.github.com>
2021-01-15 17:59:59 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const arguments = process.argv.slice(2);
+const fs = require("fs");
+
+let port = 3000;
+
+for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i].includes("=")) {
+        const segment = arguments[i].split("=");
+        switch (segment[0]) {
+            case "--port":
+                port = segment[1];
+                break;
+            default:
+                console.error("Only accept '--port=number'");
+                break;
+            }
+    } else {
+        console.log(`User arguments doesn't include '=', default to port:${port}`)
+    }
+}
+
+function writeFilePromise(path, data) {
+    return new Promise((resolve, rejects) => {
+        fs.writeFile(path, data, err => {
+            if (err) {
+                throw err;
+            } else {
+                resolve(`Successfully write at ${path}`);
+            }
+        })
+    })
+}
+
+app.get("/", (req, res) => {
+    res.json(req.query);
+})
+
+app.post("/", (req, res) => {
+        let data = "";
+        req.on("data", chunk => data += chunk);
+        req.on("end", async () => {
+            if (req.headers['content-type'] === "application/json") {
+                const dataObj = JSON.parse(data);
+                const {name, age, gender} = dataObj;
+                const dataJSON = JSON.stringify({name, age, gender}, null, 2);
+                const processData = await writeFilePromise("./user.json", dataJSON);
+                console.log(processData);
+            }
+        })
+        res.end()
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening at ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
